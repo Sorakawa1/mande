ce3c895a07d4d3dbaab8025ce72b2db1946b2709
ccgg1997 <89625031+ccgg1997@users.noreply.github.com>
2023-01-29 20:59:12 -0500

    login

--- src/routes/login.js
+++ src/routes/login.js
@@ -20,4 +20,57 @@ router1.get('/login_cliente', (req, res, next) => {
     res.render('index_login_clientes.ejs');
 })
 
+router1.post('/login_cliente', async(req, res, next) => {
+   
+    const clientes= await getDataFromAPI(req.body.email);
+    const key = 'error';
+    var hasKey = (clientes[key] !== undefined);
+
+    if(hasKey || !req.body.email|| !req.body.password){
+        console.log('datos incorrectos');
+        res.render('index_login_clientes.ejs');
+    }
+    else
+    { 
+        console.log('entro en el else');
+        const hash= (req.body.password);
+        console.log(hash);
+        const contrasena=(clientes[0].contrasena).toString().trim();
+        const rehash= await bcrypt.compare(hash,contrasena);
+        console.log(rehash);
+
+        if(rehash)
+        {
+           
+            const hash = await bcrypt.hash('1234',8);
+            const rehash= await bcrypt.compare('1234','h');
+            console.log(hash + " /n " + rehash);
+            for (i=0; i<clientes.length; i++){
+                console.log(clientes[i].nombre);
+            }
+            res.render('perfil_cliente.ejs');
+        }
+        else    {
+            console.log('datos incorrectos');
+            res.render('index_login_clientes.ejs');
+        }
+    }
+    
+})
+
+async function getDataFromAPI(celtrabajador) {
+    try {
+        const url= 'http://localhost:5000/api/client/'+celtrabajador
+        const response = await fetch(url);
+        const data = await response.json();
+        
+        //console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
 module.exports = router1;
