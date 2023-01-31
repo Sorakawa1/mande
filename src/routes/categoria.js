80a4a295b7d4d09c0f6c8632f2deb70755d94d83
JuanCamilo <juan_kamilo03@hotmail.com>
2023-01-31 03:44:01 -0500

    barra de busqueda

--- src/routes/categoria.js
+++ src/routes/categoria.js
@@ -1,6 +1,7 @@
 // Se encargara de almacenar las rutas principales
 const express = require('express');
 const routecateg = express.Router(); //Se definen las rutas del servidor
+const bcrypt = require('bcrypt');
 
 routecateg.get('/categorias', (req, res, next) => {
     res.render('categoria.ejs');  
@@ -10,12 +11,27 @@ routecateg.post('/categorias', (req, res, next) => {
     res.send('cliente');
 })
 
-routecateg.get('/resultado', (req, res, next) => {
-    res.render('resultados.ejs');  
+routecateg.get('/resultado', async(req, res, next) => {
+    const valor=req.query.buscar;
+    const url= 'http://localhost:5000/api/workerView/'+valor;
+    const trabajo= await getDataFromAPI(url);
+    console.log(trabajo);
+    res.render('resultados.ejs', {trabajo});  
 })
 
 routecateg.post('/resultado', (req, res, next) => {
     res.send('cliente');
 })
+
+async function getDataFromAPI(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        //console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 module.exports = routecateg;
--- src/routes/clientes.js
+++ src/routes/clientes.js
@@ -7,8 +7,23 @@ routcliente.get('/registrar_cliente', (req, res, next) => {
     res.render('index_registro_clientes.ejs');  
 })
 
-routcliente.post('/registrar_cliente', (req, res, next) => {
-    res.send('Ingresados correctamente');
+
+routcliente.post('/registrar_cliente', function(req, res) {
+  let sampleFile;
+  let path;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No hay nada aqui.');
+      }
+    
+      sampleFile = req.files.sampleFile;
+      path = __dirname+'/img/'+sampleFile.name;
+      sampleFile.mv(path, function(err) {
+        if (err)
+          return res.status(500).send(err);
+    
+        res.send('Si cargo el archivo');
+      });
 })
 
 module.exports = routcliente;
